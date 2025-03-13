31a604a6a9aa1a5d5c280e260a89ffd72ba0c132
gayathribalraj <gayathri.balraj@sysarcinfomatix.com>
2025-03-13 12:02:15 +0530

    createEmployeeArrayFromPersonArray

--- createEmp.js
+++ createEmp.js
@@ -1,12 +1,34 @@
 
+// Create array
+
+let person = [] ;
+
+for(let i = 1 ; i<=50 ; i++)
+    {
+        person.push("person"+i)
+    }
+    console.log(person)
 
 
-let employees = [] ;
 
-let employee = person.map((index,employees) => {
+    // Create Employee array from person array 
+
+// let employees = [] ;
+
+// let employee = person.map((index,employees) => {
     
-    let id = index ;
-    let employeeName = employees ;
-})
+//     let id = index ;
+//     let employeeName = employees ;
+// })
 
-console.log(employees);
+// console.log(employees);
+
+let employee = person.map((person,index) =>
+    ( {
+    
+    id : index +1 ,
+    EmployeeName :  person
+}));
+
+console.log(employee);
+
